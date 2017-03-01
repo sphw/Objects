c5dd25712f04382860f19c82c0f33196b58d8861
Sascha Wise <me@saschawise.com>
2017-02-28 21:25:32 -0800

    Initial Commit

---
+++ Objects/Objects.h
@@ -0,0 +1,19 @@
+//
+//  Objects.h
+//  Objects
+//
+//  Created by Sascha Wise on 2/28/17.
+//  Copyright © 2017 Sascha Wise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Objects.
+FOUNDATION_EXPORT double ObjectsVersionNumber;
+
+//! Project version string for Objects.
+FOUNDATION_EXPORT const unsigned char ObjectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Objects/PublicHeader.h>
+
+
